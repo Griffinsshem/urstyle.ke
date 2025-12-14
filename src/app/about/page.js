c14461037532760af82e700902c0f0ff8b0af0db
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-12-14 21:48:07 +0300

    feat(about): update font color

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -198,7 +198,7 @@ function FeatureCard({ title, desc }) {
   return (
     <div className="bg-white p-8 shadow-sm rounded-2xl border border-gray-900">
       <h3 className="font-bold text-xl mb-3">{title}</h3>
-      <p className="text-gray-600">{desc}</p>
+      <p className="text-gray-900">{desc}</p>
     </div>
   );
 }
